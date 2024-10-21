e044673007cadeab66115def364570f8b341048b
ANJAN <devopsanjan700@gmail.com>
2024-10-21 18:22:04 +0530

    Update lambda-deploy.ps1

--- lambda-deploy.ps1
+++ lambda-deploy.ps1
@@ -1,13 +1,19 @@
-# Package Lambda function
-Write-Host "Packaging Lambda function..."
-Compress-Archive -Path index.js -DestinationPath function.zip
+# Check if function.zip already exists and delete it if so
+if (Test-Path -Path "function.zip") {
+    Remove-Item -Path "function.zip" -Force
+}
 
-# Deploy to AWS Lambda
-Write-Host "Deploying to AWS Lambda..."
+# Package the Lambda function
+Write-Host "Packaging Lambda function..."
+Compress-Archive -Path "index.js" -DestinationPath "function.zip"
+
+# Set AWS Lambda function name and region from environment variables
 $functionName = $env:AWS_LAMBDA_FUNCTION_PROD
 $region = $env:AWS_REGION
 
-& $awsCliPath lambda update-function-code `
+# Update AWS Lambda function code
+Write-Host "Deploying to AWS Lambda..."
+& aws lambda update-function-code `
     --function-name $functionName `
     --zip-file fileb://function.zip `
     --region $region `
