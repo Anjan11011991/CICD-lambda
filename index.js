92ec32d47278effc914baab616eade5549dd31d5
ANJAN <devopsanjan700@gmail.com>
2024-10-24 15:42:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ exports.handler = async (event) => {
         headers: {
             "Content-Type": "text/plain"
         },
-        body: "Hello India123"
+        body: "Hello India123456"
     };
 };
